82e3b8d462be5f4c61b3a7e993be9da5a4e567d5
Noa R <noa.ramon01@gmail.com>
2020-12-02 23:02:27 +0200

    updates

--- setup.sh
+++ setup.sh
@@ -4,10 +4,14 @@
 
 # install Pipenv, the -n option makes sudo fail instead of asking for a 
 # passoword if we don't have sufficient privileges to run it
+sudo -n dnf install -y pipenv
 
 cd /vagrant
 # Install dependencies with Pipenv
 pipenv sync
+
+# Run database migrations
+pipenv run python manage.py migrate
 
 # run our app. Nohup and "&" are used to let the setup script finish
 #while our app stays up. The app logs will by collected in nohup.output
