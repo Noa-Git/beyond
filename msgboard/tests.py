be993dba0bb891b8170d6be6134ead4cbcd4b488
Noa R <noa.ramon01@gmail.com>
2020-12-20 19:01:09 +0200

    flake8

--- msgboard/tests.py
+++ msgboard/tests.py
@@ -1,9 +1,7 @@
-def test_with_authenticated_client(client,
-django_user_model):
- username = "admin"
- password = "123456"
- Django_user_model. \
- objects.create_user(username=username,
- password=password)
- # Use this method and assert response
- client.get(“”)
+def test_with_authenticated_client(client, django_user_model):
+    username = "admin"
+    password = "123456"
+    Django_user_model. \
+    objects.create_user(username=username, password=password)
+    # Use this method and assert response
+    client.get("")
