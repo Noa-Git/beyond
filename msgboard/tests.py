b4d5a591d708a975bda253b0e49c5453f9716835
Noa R <noa.ramon01@gmail.com>
2020-12-20 18:26:29 +0200

    bug fixes

--- msgboard/tests.py
+++ msgboard/tests.py
@@ -1,14 +1,10 @@
-# import pytest
-
 def test_with_authenticated_client(client, django_user_model):
-    username = "admin"
-    password = "123456"
-    Django_user_model.objects.create_user(username=username, password=password)
-    # Use this method and assert response:
-    # client.login(username=username, password=password)
-    response = client.get('/')
-    assert response.status_code == 302
-    response = client.get(response.url)
-    assert response.status_code == 200
-    response = client.get('/accounts/logout')
-    assert response.status_code == 301
+ username = "admin"
+ password = "123456"
+ django_user_model.objects.create_user(username=username, password=password)
+ response = client.get('/')
+ assert response.status_code == 302
+ response = client.get(response.url)
+ assert response.status_code == 200
+ response = client.get('/accounts/logout')
+ assert response.status_code == 301
