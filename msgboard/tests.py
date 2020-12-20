1915627170acd6455718ae3caaea7bc9131d7174
Noa R <noa.ramon01@gmail.com>
2020-12-20 18:50:44 +0200

    flake8 change

--- msgboard/tests.py
+++ msgboard/tests.py
@@ -1,10 +1,9 @@
-def test_with_authenticated_client(client, django_user_model):
+def test_with_authenticated_client(client,
+django_user_model):
  username = "admin"
  password = "123456"
- django_user_model.objects.create_user(username=username, password=password)
- response = client.get('/')
- assert response.status_code == 302
- response = client.get(response.url)
- assert response.status_code == 200
- response = client.get('/accounts/logout')
- assert response.status_code == 301
+ Django_user_model. \
+ objects.create_user(username=username,
+ password=password)
+ # Use this method and assert response
+ client.get(“”)
